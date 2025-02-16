069c98c6995b349e396475f02cc7cec772c8dfb2
RémiViard <remi.gabriel.viard@gmx.fr>
2025-02-16 21:38:33 +0100

    Base player click
    I go dodo

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    InputAction select;
+    void Start()
+    {
+        select = InputSystem.actions.FindAction("Select");
+        select.performed += Select;
+    }
+
+    private void Select(InputAction.CallbackContext obj)
+    {
+        //Debug.Log(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
