607958213ad34c6997b44f0a3ba15c539022a95c
Diego Imbert <diegoimbert@pm.me>
2025-02-17 14:32:40 +0100

    objects sliding to right

---
+++ Assets/Script/IngredientSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+    [SerializeReference] private List<GameObject> _ingredients;
+    [SerializeField] private float _maxTime = 1.5f;
+
+    private float _timer = 0f;
+
+    void Start()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        SpawnRandomIngredient();
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _maxTime) {
+            SpawnRandomIngredient();
+            _timer = 0f;
+        }
+    }
+
+    private void SpawnRandomIngredient() {
+        var prefab = _ingredients[Random.Range(0, _ingredients.Count-1)];
+        var instance = Instantiate(prefab, transform.position, Quaternion.identity);
+        instance.AddComponent<MoveIngredientOnTreadmill>();
+    }
+}
