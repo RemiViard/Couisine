8193b90f371ba72db51f9d8226859173c7b4ee64
Diego Imbert <diegoimbert@pm.me>
2025-02-18 12:25:40 +0100

    Refactoring i will not explain

--- Assets/Script/Stage1Basket.cs
+++ Assets/Script/Stage1Basket.cs
@@ -1,21 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Basket : MonoBehaviour
+public class Stage1Basket : MonoBehaviour
 {
-    private List<GameObject> basket = new List<GameObject>();
+    public List<GameObject> basket = new List<GameObject>();
 
     public float offset = 2.0f;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void AddItem(GameObject obj) {
         obj.transform.position = transform.position + Vector3.right * offset * basket.Count;
