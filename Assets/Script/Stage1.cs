a21e91b140325606bc11ae3aa02fd86e0dad546e
Diego Imbert <diegoimbert@pm.me>
2025-02-18 13:12:04 +0100

    Move basket to stage2

--- Assets/Script/Stage1.cs
+++ Assets/Script/Stage1.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class Stage1 : MonoBehaviour
 {
     [SerializeField] Stage1Basket basket;
+    [SerializeField] GameObject stage2;
 
     void Start()
     {
-        
+        basket.onAdd += OnBasketUpdate;
     }
 
+    void OnBasketUpdate() {
+        // Condition to go to stage2
+        if (basket.transform.childCount == 4) {
+            GoToStage2();
+        }
+    }
+
+    void GoToStage2() {
+        basket.transform.parent = stage2.transform;
+        gameObject.SetActive(false);
+        stage2.SetActive(true);
+    }
 }
--- Assets/Script/Stage1Basket.cs
+++ Assets/Script/Stage1Basket.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,8 +6,11 @@ public class Stage1Basket : MonoBehaviour
 {
     public float offset = 2.0f;
 
+    public event Action onAdd;
+
     public void AddItem(GameObject obj) {
         obj.transform.position = transform.position + Vector3.right * offset * gameObject.transform.childCount;
         obj.transform.parent = gameObject.transform;
+        onAdd?.Invoke();
     }
 }
