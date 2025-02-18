c007b3e8464043f15ceb16f58fedcb0ed0767722
Diego Imbert <diegoimbert@pm.me>
2025-02-18 12:11:31 +0100

    Remove items from treadmill on disable

--- Assets/Script/IngredientSpawner.cs
+++ Assets/Script/IngredientSpawner.cs
@@ -11,6 +11,8 @@ public class IngredientSpawner : MonoBehaviour
 
     private float _timer = 0f;
 
+    private HashSet<GameObject> _currentlyOnTreadmill = new HashSet<GameObject>();
+
     void Start()
     {
         Random.InitState(System.DateTime.Now.Millisecond);
@@ -29,15 +31,24 @@ public class IngredientSpawner : MonoBehaviour
     private void SpawnRandomIngredient() {
         var prefab = _ingredients[Random.Range(0, _ingredients.Count-1)];
         var instance = Instantiate(prefab, transform.position, Quaternion.identity);
+        _currentlyOnTreadmill.Add(instance);
         
         var moveScript = instance.AddComponent<MoveIngredientOnTreadmill>();
         moveScript._speed = _speed;
 
         var clickableScript = instance.AddComponent<Clickable>();
         clickableScript.SetOnClick(() => {
+            _currentlyOnTreadmill.Remove(instance);
             Destroy(moveScript);
             Destroy(clickableScript);
             basket.AddItem(instance);
         });
+    }
+
+    void OnDisable()
+    {
+        foreach (var obj in _currentlyOnTreadmill) {
+            Destroy(obj);
+        }
     }
 }
