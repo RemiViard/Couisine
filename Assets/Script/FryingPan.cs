563733e1f98279c2cd01e2978e74629788f807d2
RémiViard <remi.gabriel.viard@gmx.fr>
2025-02-21 01:15:53 +0100

    Raiponce

--- Assets/Script/FryingPan.cs
+++ Assets/Script/FryingPan.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class FryingPan : MonoBehaviour
 {
-    Vector2 lastMousePos;
+    Vector3 lastMousePos;
+    [SerializeField] Transform children;
+    [SerializeField] float rotationSensitivity;
+    [SerializeField] float rotationClamp;
     private void Start()
     {
-        
+        lastMousePos = Input.mousePosition;
     }
     private void Update()
     {
-        
+        if (lastMousePos != Input.mousePosition)
+        {
+            Vector2 input = Input.mousePosition - lastMousePos;
+            input *= Time.deltaTime * rotationSensitivity;
+            float testedZ = (transform.eulerAngles.z + input.x);
+            if(testedZ <= rotationClamp || testedZ >= 360 - rotationClamp)
+            {
+                transform.Rotate(transform.forward, input.x, Space.World);
+            }
+            float testedX = (children.eulerAngles.x + input.y);
+            if(testedX <= rotationClamp || testedX >= 360 - rotationClamp)
+            {
+                children.Rotate(children.right, input.y, Space.World);
+            }
+            lastMousePos = Input.mousePosition;
+
+        }
     }
 }
